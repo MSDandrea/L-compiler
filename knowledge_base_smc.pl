7111d7ea3bff3d0a06046b632e87e506e1cf7b56
MSDandrea <matheusdandrea@hotmail.com>
2017-09-15 02:54:15 -0300

    MAY THE ODDS BE EVER IN YOUR FAVOR

--- knowledge_base_smc.pl
+++ knowledge_base_smc.pl
@@ -1,7 +1,9 @@
+:- op(0, xfx, =>).
+:- op(800, xfx, =>).
 % -- grammar
 atom_is_lower(N) :-
     atom_chars(N, [L]),
-    char_type(L, lower).
+    char_type(L, lower);fail.
 
 
 
@@ -19,7 +21,7 @@ mult_op --> [*].
 literal --> [I], {integer(I)}.
 literal --> [C], {char_type(C,digit)}.
 variable --> [V], {atom_is_lower(V)}.
-truth_value --> [T],{member(T,[true,false])}.
+truth_value--> [T],{member(T,[true,false])}.
 
 
 boolean_expression --> truth_value.
@@ -44,7 +46,7 @@ pop([X|List],X,List).
 push(X,List,[X|List]).
 
 % memory ops_dic
-update_memory(K, V, Dict_in, Dict_out):- put_dict([K=V], Dict_in, Dict_out).
+update_memory(K, V, Dict_in, Dict_out):-put_dict([K=V], Dict_in, Dict_out).
 read_memory(K,Dict,Expected):- get_dict(K,Dict,Expected). 
 
 %SMC transitions tran(initial_S, initial_M, initial_C, final_S, final_M, final_C).
@@ -55,15 +57,63 @@ write_smc_state(S,M,C):-
     write(M),
     nl,
     write(C),nl.
-tran(S, M, [L1,OP,L2| C], [L1, L2 | S], M, [OP | C]):- expression([L1,OP,L2],[]), write_smc_state(S, M, [L1,OP,L2| C]), write_smc_state([L1, L2 | S], M, [OP | C]). %expression_tran
-tran(S, M, [K| C], [K|S], M, C):- literal([K],[]). %literal_tran
-tran(S,M,[K|C],[V|S],M,C):- read_memory(K,M,V). %var_tran
 
-%eval of SMC states
-eval([L1,L2 | _], _, [OP | _], Result):- add_op([OP],[]), literal([L1],_), literal([L2],_), Result is L1 + L2.
-eval([L1,L2 | _], _, [OP | _], Result):- sub_op([OP],[]), literal([L1],_), literal([L2],_), Result is L1 - L2.
-eval([L1,L2 | _], _, [OP | _], Result):- mult_op([OP],[]), literal([L1],_), literal([L2],_), Result is L1 * L2.
+%%%%%%% COMMANDS %%%%%%%%%%%%%%%%%%%%%%
+(S,M,[nil| C]) => (S,M,C).
 
-%"Compile"
-compile_list(List,Result):- expression(List,_), tran([],_,List,S,M,C), eval(S,M,C,Result).
+(S,M,[Exp | C]) => (S,M,[E1,E2|C]):-
+    compound(Exp),
+    Exp =.. [OP,E1,E2],
+    member(OP,[;]).
+
+%%%%%%% BOOLEAN EXPRESSION %%%%%%%%%%%%
+(S,M,[Bool|C]) => ([Bool | S], M , C):-
+    truth_value([Bool],_).   	
+
+([L1,L2 |S],M,[OP|C]) => ([Value | S],M,C):-
+    literal([L1],_),literal([L2],_),
+    member(OP,[=]),
+    (call(L1=:=L2) ->   Value=true; Value=false).
+
+
+%%%%%%% EXPRESSIONS %%%%%%%%%%%%%%%%%%%
+([L1,L2 | S], M, [OP|C]) => ([Result | S],M,C):-
+	literal([L1],_),literal([L2],_),
+    member(OP,[+,-,*]),
+	Expression =.. [OP,L1,L2],
+	Result is Expression.
+
+
+(S,M,[Exp | C]) => (S,M,[E1,E2,OP|C]):-
+    compound(Exp),
+    Exp =.. [OP,E1,E2],
+    member(OP,[+,-,*,=]).
+    
+
+(S,M,[Exp | C]) => ([E1|S],M,[E2,OP|C]):-
+    compound(Exp),
+    Exp =.. [OP,E1,E2],
+    variable([E1],_),
+    member(OP,[:=]).
+
+([Lit,Var|S],Minit,[:= | C]) => (S,Mfin,C):-
+    literal([Lit],_),
+    variable([Var],_),
+    update_memory(Var,Lit,Minit,Mfin).
+
+(S,M,[Literal | C]) => ([Literal | S],M,C):-
+    literal([Literal],_).
+
+(S, M, [Var | C]) => ([Val | S], M, C):-
+    variable([Var],_),
+    read_memory(Var,M,Val).
+
+eval(Input, Output) :-
+	Input => Mid,
+	!,
+	eval(Mid, Output).
+eval(Output, Output).
+
+run(Program,Memory):-
+    eval(([],m{},[Program]), ([],Memory,[])).
 
