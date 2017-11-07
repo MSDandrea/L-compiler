946f0011362c00b050ed2e24b558878f015b8c72
MSDandrea <matheusdandrea@hotmail.com>
2017-11-06 22:53:37 -0200

    Add a factorial test file and a feeder to the compiler

---
+++ start.pl
@@ -0,0 +1,12 @@
+:- [smc_machine].
+main:-
+    	write('Please type L filename:'),
+		nl,
+		read(user_input,X),
+		with_output_to(atom(A), write(X)),
+		open(A,read,File),
+		read_string(File, "", "\r\t ", End, String), nl,
+		split_string(String, "\n", "", String2),
+		atomic_list_concat(String2, '', Atom), atom_string(Atom, Programa),
+		compile_run(Programa,M),
+		write(M),nl.
